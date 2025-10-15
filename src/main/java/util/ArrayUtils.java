6cb0dff1429bf1544b112b1dcf37c3ab323d1c55
nrbdi <242250@astanait.edu.kz>
2025-10-15 17:11:33 +0500

    2 commit

--- src/main/java/util/ArrayUtils.java
+++ src/main/java/util/ArrayUtils.java
@@ -1,4 +1,47 @@
 package util;
 
+import java.util.Random;
+
 public class ArrayUtils {
+    private static final Random random = new Random();
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void shuffle(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            swap(arr, i, j);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right, int pivotIndex) {
+        int pivotValue = arr[pivotIndex];
+        swap(arr, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (arr[i] < pivotValue) {
+                swap(arr, storeIndex, i);
+                storeIndex++;
+            }
+        }
+        swap(arr, right, storeIndex);
+        return storeIndex;
+    }
+
+    public static void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
--- src/main/java/util/Point.java
+++ src/main/java/util/Point.java
@@ -1,4 +1,28 @@
 package util;
 
-public class Point {
+public class Point implements Comparable<Point> {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceTo(Point other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public int compareTo(Point other) {
+        int cmp = Double.compare(x, other.x);
+        return cmp != 0 ? cmp : Double.compare(y, other.y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%.2f, %.2f)", x, y);
+    }
 }
